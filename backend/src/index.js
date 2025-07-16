8e6f131c356b955510b773d33980621d613177ad
Jagannath741 <jagannathpatra718@gmail.com>
2025-07-16 13:11:19 +0530

    Updated Files

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,10 +32,10 @@ const __dirname = path.dirname(__filename);
 
 // Serve frontend static files in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
     });
 }
 
