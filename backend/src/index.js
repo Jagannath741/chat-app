343ba19840715135d06fc0f8de5e4b93938a7256
Jagannath741 <jagannathpatra718@gmail.com>
2025-07-18 17:58:06 +0530

    Upload Files

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ dotenv.config();
 app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // frontend dev origin
+    origin: process.env.NODE_ENV === "production" ? "https://chat-app-5-tisa.onrender.com" : "http://localhost:5173",
     credentials: true
 }));
 
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Start server and connect DB
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 6060;
 
 server.listen(PORT, () => {
     console.log(`🚀 Server running at http://127.0.0.1:${PORT}`);
