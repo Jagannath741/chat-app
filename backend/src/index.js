1104a20ad24ba51d153144dcc972bfb47a3ab9e1
Jagannath741 <jagannathpatra718@gmail.com>
2025-07-19 14:48:32 +0530

    Upload latest File

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,24 +17,12 @@ dotenv.config();
 // Middleware
 app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://chat-app-7-zy87.onrender.com",
-    "https://chat-app-8-czrx.onrender.com"
-];
-
-app.use(cors({
-    origin: function(origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
